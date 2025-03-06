0d87a384b03bd421a8742020d7e1e901745d737f
Aparna Vinod <aparnavinod083@gmail.com>
2025-03-06 17:24:22 +0530

    task controller and router added

--- Router/router.js
+++ Router/router.js
@@ -1,7 +1,41 @@
 
 const express = require('express');
+const { addAllUserstask } = require('../Controller/taskController');
 const router = express.Router();
 
+//add all tasks
+router.post('/addAllUsersTask',jwtMiddleware,taskController.addAllUserstask)
+
+//add tasks from managers to employees
+
+router.post('/addTaskToEmployees/:id',jwtMiddleware,taskController.addEmployeetask)
+//get all task details 
+
+router.get('/getAll Task/:id',jwtMiddleware,taskController.getAllUserTasks)
+
+//get all task assigned to managers or employes
+
+router.get('/getManagersTask/:id',jwtMiddleware,taskController.getAllUserTasks)
+
+//task assigned  by manger to employee .view by manager
+
+router.get('/getManagerAssignedTask/:id',jwtMiddleware,taskController.getManagerAssignedTask)
+
+//task of employees
+router.get('/getEmployeesTask',jwtMiddleware,taskController.getEmployeesTask)
 
 
+//task assigned  by admin to manager  --manger side
+
+
+router.get('/getManagerAssignedTaskByAdmin',jwtMiddleware,taskController.getManagerAssignedTaskByAdmin)
+// Update task status
+router.put('/status/edit/:id',jwtMiddleware,taskController.updateTaskStatus)
+
+//edit task
+router.put('/tasks/edit/:id',jwtMiddleware,taskController.editTask)
+
+
+//delete
+router.delete('/tasks/delete/:id',jwtMiddleware,taskController.deleteTask)
 module.exports = router;
