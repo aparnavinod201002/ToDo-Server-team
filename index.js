f6a9326d60a20fc06473a943f0d5a324d3ec55c9
Aparna Vinod <aparnavinod083@gmail.com>
2025-03-06 15:14:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
+const router =require('./Router/router')
+ require('./DB/connection')
 
 
 const ToDoServer =express()
 
 ToDoServer.use(cors())
+ToDoServer.use(express.json())
+ToDoServer.use(router)
 
 const PORT = 3000 || process.env.PORT
 ToDoServer.listen(PORT,()=>{
